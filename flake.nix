a4279a3ce0e0d63719a88e11d5335b28cdbece99
Georges Alkhouri <georges.alkhouri@gmail.com>
2025-02-16 10:17:55 +0100

    feat: update flake.nix inputs 🚀🛠
    Reorder inputs for better clarity 😃.
    Replace the old nixpkgs URL with the official release URL and add a
    dedicated entry for devenv inputs 🍰.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Centralized input hub for devenv projects";
 
   inputs = {
-    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-devenv.url = "github:cachix/devenv-nixpkgs/rolling";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-python.url = "github:cachix/nixpkgs-python";
     nixpkgs-python.inputs.nixpkgs.follows = "nixpkgs";
